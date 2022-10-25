c54ec22302479a175577f663c30a862e60042763
Farhan Elias <faarhaan10@gmail.com>
2022-10-26 03:48:55 +0600

    courses page done

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom';
+import LeftSide from './LeftSide';
 
 const Courses = () => {
     return (
         <Container>
             <Row>
-                <Col lg="4">
-                    <h3>Left side</h3>
-
+                <Col lg="3">
+                    <LeftSide />
                 </Col>
 
-                <Col lg="8">
-
-                    <h2>
-                        Right side
-                    </h2>
+                <Col lg="9">
+                    <Outlet />
                 </Col>
             </Row>
         </Container>
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,10 @@ import Main from "../Layout/Main";
 import Authentication from "../Pages/Authentication/Authentication";
 import Blog from "../Pages/Blog/Blog";
 import Courses from "../Pages/Courses/Courses";
+import About from "../Pages/Courses/RightSide/About";
+import MyCourses from "../Pages/Courses/RightSide/MyCourses";
+import Payment from "../Pages/Courses/RightSide/Payment";
+import Profile from "../Pages/Courses/RightSide/Profile";
 import Home from "../Pages/Home/Home";
 
 export const routes = createBrowserRouter([
@@ -25,6 +29,24 @@ export const routes = createBrowserRouter([
             {
                 path: '/courses',
                 element: <Courses />,
+                children: [
+                    {
+                        index: true,
+                        element: <MyCourses />
+                    },
+                    {
+                        path: 'profile',
+                        element: <Profile />
+                    },
+                    {
+                        path: 'about',
+                        element: <About />
+                    },
+                    {
+                        path: 'payment',
+                        element: <Payment />
+                    },
+                ]
             },
         ]
     }
