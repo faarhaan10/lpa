229d7e706ca784f2f0a6b1cf4aeabf1ce1a15203
Farhan Elias <faarhaan10@gmail.com>
2022-10-27 01:21:36 +0600

    done

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -2,12 +2,35 @@ import React from 'react';
 import Navigation from '../Pages/Navigation/Navigation';
 import Footer from '../Pages/Shared/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const Main = () => {
+    AOS.init({
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 100,
+    });
     return (
         <div>
             <Navigation />
             <Outlet></Outlet>
             <Footer></Footer>
+            <ToastContainer
+                position="top-right"
+                autoClose={1500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
         </div>
     );
 };
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,12 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Authentication from "../Pages/Authentication/Authentication";
 import Blog from "../Pages/Blog/Blog";
+import Checkout from "../Pages/Checkout/Checkout";
 import Courses from "../Pages/Courses/Courses";
 import About from "../Pages/Courses/RightSide/About";
 import MyCourses from "../Pages/Courses/RightSide/MyCourses";
 import Payment from "../Pages/Courses/RightSide/Payment";
 import Profile from "../Pages/Courses/RightSide/Profile";
+import SingleCourse from "../Pages/Courses/RightSide/SingleCourse";
 import Home from "../Pages/Home/Home";
+import PrivateRoute from "../Pages/PrivateRoute/PrivateRoute";
+
+//server url
+const url = 'http://localhost:4000';
 
 export const routes = createBrowserRouter([
     {
@@ -27,12 +33,29 @@ export const routes = createBrowserRouter([
                 element: <Blog />,
             },
             {
+                path: '/checkout/:id',
+                loader: async ({ params }) => {
+                    return fetch(`${url}/courses/${params.id}`)
+                },
+                element: <PrivateRoute><Checkout /></PrivateRoute>,
+            },
+            {
+                path: '/course/:id',
+                loader: async ({ params }) => {
+                    return fetch(`${url}/courses/${params.id}`)
+                },
+                element: <SingleCourse />,
+            },
+            {
                 path: '/courses',
                 element: <Courses />,
                 children: [
                     {
                         index: true,
-                        element: <MyCourses />
+                        element: <MyCourses />,
+                        loader: async () => {
+                            return fetch(`${url}/courses`)
+                        }
                     },
                     {
                         path: 'profile',
